a7a47ed295d09e02c8ed7e5262c3a4003cf98ef8
Jimmy <miki3867@gmail.com>
2021-06-27 17:19:47 +0200

    Add requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+import json
+
+secret = open('secret.json')
+secret = json.load(secret)
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://"
+
+app.config["MONGO_URI"] = secret['connection-string']
+mongo = PyMongo(app)
 
 if __name__ == '__main__':
     app.run()
