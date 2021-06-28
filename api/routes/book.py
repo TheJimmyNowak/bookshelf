a24c5bace4badae21e2ac1d2e9ac1a36bbdbe405
Jimmy <miki3867@gmail.com>
2021-06-28 22:27:19 +0200

    Create endpoints

--- api/routes/book.py
+++ api/routes/book.py
@@ -1,18 +1,30 @@
 import json
+import logging
 
-from flask import Blueprint, jsonify, Response
+from bson import ObjectId
+from flask import Blueprint, jsonify, Response, request
 from api.models import mongo
 from api.util.JSONEncoder import JSONEncoder
 
 book = Blueprint('book', __name__)
+logger = logging.getLogger("books")
 
 
-@book.route('/api/book', methods=['GET'])
-def get_book() -> Response:
-    """
-    It's just example. To redesign in future
-    :return:
-    """
-    res = mongo.db.books.find_one()
+@book.route('/api/book/<book_id>', methods=['GET'])
+def get_book(book_id: str) -> Response:
+    res = mongo.db.books.find_one({"_id": ObjectId(book_id)})
     res = json.loads(JSONEncoder().encode(res))
     return jsonify(res)
+
+
+@book.route('/api/book', methods=["POST"])
+def add_book() -> Response:
+    content = request.json
+
+    if len(content['name']) < 0 or len(content['author']) < 0:
+        logger.debug("Endpoint called without proper body")
+        return Response(status=400)
+
+    mongo.db.books.insert_one(content)
+
+    return Response(status=500)
--- test/test_book.py
+++ test/test_book.py
@@ -1,11 +1,16 @@
 from app import create_app
 from unittest import TestCase
+from unittest.mock import Mock, patch
 
 
 class Test(TestCase):
     def setUp(self) -> None:
         self.app = create_app('../secret.json').test_client()
 
-    def test_get_book(self):
-        result = self.app.get('/api/book')
-        self.assertEqual(result.status_code, 200)
+    @patch('api.routes.book.mongo')
+    def test_get_book(self, mongo_mock):
+        mongo_mock.db.books.find_one.return_value = {}
+        result = self.app.get('/api/book/1')
+
+        self.assertEqual(mongo_mock.called, True)
+        self.assertEqual(result, {})
