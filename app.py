827a5a15cef5401790d44decf764efe6d459ceba
Jimmy <miki3867@gmail.com>
2021-06-30 16:13:29 +0200

    Change app

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 from flask import Flask
 import json
+import os
 
 
 def create_app(secret_path='secret.json'):
     app = Flask(__name__)
+
+    from api.routes.book import book
+    app.register_blueprint(book)
+
+    if not os.path.exists(secret_path):
+        print("Secrets are not setted up")
+        return app
+
     with open(secret_path) as secret:
         secret = json.load(secret)
 
-        from api.routes.book import book
         from api.models import mongo
 
-        app.register_blueprint(book)
         app.config["MONGO_URI"] = secret['connection-string']
 
     mongo.init_app(app)
