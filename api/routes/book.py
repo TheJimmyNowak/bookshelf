12aaff68bf4e5219b4b741b6940e570d76d27eba
Jimmy <miki3867@gmail.com>
2021-06-30 15:32:55 +0200

    Add get_book_by_localization

--- api/routes/book.py
+++ api/routes/book.py
@@ -5,30 +5,53 @@ from bson import ObjectId
 from flask import Blueprint, jsonify, Response, request
 from api.models import mongo
 from api.util.JSONEncoder import JSONEncoder
+from bson.json_util import dumps
 
 book = Blueprint('book', __name__)
 logger = logging.getLogger("books")
 
 
 @book.route('/api/book/<book_id>', methods=['GET'])
-def get_book(book_id: str) -> Response:
-    res = mongo.db.books.find_one({"_id": ObjectId(book_id)})
-    res = json.loads(JSONEncoder().encode(res))
-    return jsonify(res)
+def get_book_id(book_id: str) -> Response:
+    result = mongo.db.books.find_one({"_id": ObjectId(book_id)})
+    result = json.loads(JSONEncoder().encode(result))
+    return jsonify(result)
+
+
+@book.route('/api/book/<float:longitude>/<float:latitude>/<float:max_distance>')
+def get_book_by_localization(longitude: float, latitude: float, max_distance: float) -> Response:
+    is_longitude_correct = -180 < longitude < 180
+    is_latitude_correct = -90 < latitude < 90
+
+    if not is_longitude_correct or not is_latitude_correct:
+        return Response(status=400)
+
+    result = mongo.db.books.find({
+        "location": {
+            "$near":
+                {
+                    "$geometry": {"type": "Point", "coordinates": [longitude, latitude]},
+                    "$maxDistance": max_distance
+                }
+        }
+    })
+
+    result = dumps(result)
+    result = json.loads(result)
+    return jsonify(result)
 
 
 @book.route('/api/book', methods=["POST"])
 def add_book() -> Response:
     content = request.json
 
-    # That code kinda scares me
-    def are_required_values_passed() -> bool:
-        return not(content is None or not ("name" in content) or not ("author" in content))
+    is_required_data_passed: bool = content is not None and "name" in content and "author" in content
 
-    if not are_required_values_passed():
-        logger.debug("Endpoint called without proper body")
-        return Response(status=400)
+    if is_required_data_passed:
+        mongo.db.books.insert_one(content)
+        logging.info("{} was added".format(content))
 
-    mongo.db.books.insert_one(content)
+        return Response(status=201)
 
-    return Response(status=201)
+    logger.debug("Endpoint called without proper body")
+    return Response(status=400)
--- test/test_book.py
+++ test/test_book.py
@@ -20,6 +20,33 @@ class Test(TestCase):
         result = self.app.get('/api/book')
         self.assertEqual(result.status_code, 405)
 
+    def test_get_book_by_localization(self, mongo_mock):
+        test_entry = [{
+            "_id": {
+                "$oid": "60dc619397073050db7ad9c4"
+            },
+            "author": "Lenin",
+            "conditon": "fadf",
+            "location": {
+                "coordinates": [
+                    73.23,
+                    21.2
+                ],
+                "type": "Point"
+            },
+            "name": "dłupa"
+        }]
+
+        mongo_mock.db.books.find.return_value = test_entry
+
+        result = self.app.get('/api/book/73.23/21.2/1000000.0')
+
+        self.assertEqual(result.json, test_entry)
+        self.assertEqual(result.status_code, 200)
+
+        result = self.app.get('/api/book/190.1/42.0/10.0')
+        self.assertEqual(result.status_code, 400)
+
     def test_add_book(self, mongo_mock):
         result = self.app.post('/api/book')
         self.assertEqual(result.status_code, 400)
