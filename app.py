4eaf641709b2cc66cf085e0ccf7ed116f8ffc66c
Jimmy <miki3867@gmail.com>
2021-07-01 12:04:51 +0200

    Reapair tests

--- app.py
+++ app.py
@@ -9,15 +9,16 @@ mongo = PyMongo()
 def create_app(test: bool = False):
     app = Flask(__name__)  # pylint: disable=redefined-outer-name
 
-    secret_path = '../secret.json' if test == 1 else 'secret.json'
-    with open(secret_path) as secret:
-        secret = json.load(secret)
-        app.config['MONGO_URI'] = secret['MONGO_URI']
+    if test == 0:
+        secret_path = 'secret.json'
+        with open(secret_path) as secret:
+            secret = json.load(secret)
+            app.config['MONGO_URI'] = secret['MONGO_URI']
+
+        mongo.init_app(app)
 
     from api.routes.book import book  # pylint: disable=import-outside-toplevel,cyclic-import
-
     app.register_blueprint(book)
-    mongo.init_app(app)
 
     return app
 
